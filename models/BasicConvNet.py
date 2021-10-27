75576919e9840f3f516ac826a85462e6357d7716
priyammazumdar <priyammazumdar1@gmail.com>
2021-10-27 14:23:11 -0500

    Thinner convnet with elu activation

--- models/BasicConvNet.py
+++ models/BasicConvNet.py
@@ -10,9 +10,14 @@ class ConvNet(nn.Module):
         self.input_shape = input_shape
 
         self.conv1 = nn.Conv2d(in_channels=self.in_channels, out_channels=16, kernel_size=(3,3), padding="same")
+        self.conv1_bn = nn.BatchNorm2d(16)
         self.mp1 = nn.MaxPool2d(kernel_size=(2,2), stride=2)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3,3), padding="same")
+        self.conv2_bn = nn.BatchNorm2d(32)
         self.mp2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding="same")
+        self.conv3_bn = nn.BatchNorm2d(64)
+        self.mp3 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
 
         self.flatten_shape = None
         zero_ex = torch.zeros(input_shape).unsqueeze(0)
@@ -20,17 +25,19 @@ class ConvNet(nn.Module):
         with torch.no_grad():
             self.convolutions(zero_ex)
 
-        self.fc1 = nn.Linear(in_features=self.flatten_shape, out_features=64)
-        self.drop1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(in_features=64, out_features=1)
+        self.fc1 = nn.Linear(in_features=self.flatten_shape, out_features=1)
+        self.drop1 = nn.Dropout(0.15)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
         # self.drop2 = nn.Dropout(0.5)
-        # self.fc3 = nn.Linear(in_features=64, out_features=1)
+        self.fc3 = nn.Linear(in_features=32, out_features=1)
 
     def convolutions(self, x):
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
         x = self.mp1(x)
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
         x = self.mp2(x)
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = self.mp3(x)
 
         # Reshape for linear
         x = x.view(x.shape[0], -1)
@@ -42,9 +49,8 @@ class ConvNet(nn.Module):
 
     def forward(self, x):
         x = self.convolutions(x)
-
-        x = F.relu(self.fc1(x))
-        x = self.drop1(x)
-        x = self.fc2(x)
-
+        x = self.fc1(x)
+        # # x = self.drop1(x)
+        # x = F.elu(self.fc2(x))
+        # x = self.fc3(x)
         return x
