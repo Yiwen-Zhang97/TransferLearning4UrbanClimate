00f2a7559fce5b34a46301b9e54ea02a574e88e9
Yiwen-Zhang97 <yiwenz9@illinois.edu>
2022-08-24 15:05:23 -0500

    Add quality and cloud control

---
+++ data_builder/tfrecords2numpy_qc.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+class TFRecordsParser:
+    """
+    Initial Data Format as a tfrecord. To use with other Deep Learning Frameworks, we can convert to
+    numpy arrays for convenience.
+    """
+    def __init__(self, filepath, channels=None, label=None, image_dim=(33,33)):
+        # Load in tfrecords and variables
+        self.raw_image_dataset = tf.data.TFRecordDataset(filepath)
+        self.channels = channels
+        self.image_dim = image_dim
+        self.label = label
+
+        # Get which channels we want to extract
+        if self.channels is None:
+            self.channels = ['Red', 'Green', 'Blue', "NIR", "SWIR1"]
+
+        if self.label is None:
+            self.label = ["LST_Day_1km",'QC_Day']
+#            self.label = "LST_Day_1km"
+
+    def tfrecrods2numpy(self, clip=True):
+        records = []
+        for idx, raw_record in enumerate(self.raw_image_dataset):
+            featureset = {}
+            example = tf.train.Example()
+            example.ParseFromString(raw_record.numpy())
+            for feature in self.channels:
+                featureset[feature] = np.array(example.features.feature[feature].float_list.value).reshape(self.image_dim)
+
+#            lst = np.array(example.features.feature[self.label].float_list.value)  
+            lst = np.array(example.features.feature[self.label[0]].float_list.value)
+            QC = np.array(example.features.feature[self.label[1]].float_list.value)
+            if len(lst) == 0: # If LST doens't exist then set to false to filter out later
+                lst = False
+            else:
+                lst = lst[0]
+                
+            if len(QC) == 0: # If QC doens't exist then set to false to filter out later
+                QC = False
+            else:
+                QC = int(QC[0])            
+
+            # Build array out of featureset
+            featureset = np.array(list(featureset.values()))
+            records.append((featureset, lst, QC))
+#            records.append((featureset, lst))
+
+        return records
+
+
+class TFRecordsElevation:
+    def __init__(self, filepath, channels=None, label=None, image_dim=(33,33)):
+        self.raw_elevation_dataset = tf.data.TFRecordDataset(filepath)
+        self.channels = channels
+        self.image_dim = image_dim
+        self.label = label
+
+        self.channels = "elevation"
+
+    def tfrecrods2numpy(self, clip=True):
+        elevations = {}
+        for idx, raw_record in enumerate(self.raw_elevation_dataset):
+            feature = self.channels
+            example = tf.train.Example()
+            example.ParseFromString(raw_record.numpy())
+            elevations[idx] = np.array(example.features.feature[feature].float_list.value).reshape(self.image_dim)
+
+        return elevations
+
+if __name__ == "__main__":
+    tfe = TFRecordsElevation(filepath="/glade/work/yiwenz/AWS3D30_cropped.tfrecord")
+    tfe.tfrecrods2numpy()
